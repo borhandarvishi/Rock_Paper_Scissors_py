405c2c00f98a1edd0617af62f613472eb031c648
Borhan darvishi <borhandr23@gmail.com>
2022-07-04 17:50:21 +0430

    This is PRS code.

---
+++ PRS.py
@@ -0,0 +1,43 @@
+import random
+
+computer_win = 0
+user_win = 0
+
+options = ['rock','paper','scissors']
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q for quit: ").lower()
+    if user_input.lower() == 'q':
+        break
+    if user_input not in options:
+        continue
+    
+    random_number = random.randint(0,2)
+    # rock : 0 - paper : 1 - scissors : 2
+
+    computer_pick = options[random_number]
+    print("I picked",computer_pick + '.')
+
+    if user_input == computer_pick:
+        print('Equal!')
+        continue
+    elif user_input == 'rock' and computer_pick == 'scissors':
+        print('You won!')
+        user_win += 1
+        continue 
+    elif user_input == 'paper' and computer_pick == 'rock':
+        print('You won!')
+        user_win += 1
+        continue
+    elif user_input == 'scissors' and computer_pick == 'paper':
+        print('You won!')
+        user_win += 1
+        continue
+    
+    else:
+        print('I won!')
+    computer_win += 1
+
+print("You won",user_win,"times.")
+print("I won",computer_win,"times.")
+print('Goodbye!')
